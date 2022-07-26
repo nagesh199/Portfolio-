ab07d371e531c23dba829233e63963f68b403dc9
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-07-26 10:50:54 +0530

    add project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,6 +3,18 @@ export const projectsData = [
 
     {
         id: 1,
+        projectName: ' Tata 1mg-clone',
+        projectDesc: ' Tata 1mg provides services, including e-pharmacy, diagnostics, e-consultation and health content.',
+        tags: ["React", "Redux","css","EJS", "Node.js", "mongoDB"],
+        logos: ["fab fa-css3","fab fa-vuejs", "fas fa-database", "fab fa-js", "fab fa-node"],
+        code: 'https://github.com/nagesh199/shocking-grade-9442',
+        demo: 'https://onemgclone.vercel.app/',
+        image: "https://github.com/gautam6023/Storage-resources-/blob/main/1mg%20Storage/Banner%20Images/1st.png?raw=true"
+    },
+
+    {
+       
+        id: 2,
         projectName: 'Sports-Jam-Clone',
         projectDesc: 'A website facilitating to buying of sports equipment and apparel.',
         tags: ["React", "Redux", "Html","Css"],
@@ -12,18 +24,18 @@ export const projectsData = [
         image: "https://storage.sg.content-cdn.io/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/userimages/PAANY-R.jpg"
     },
     {
-        id: 2,
+        id: 3,
         projectName: 'Nykaa-Clone',
         projectDesc: 'An online website and application to buy beauty apparent.',
         tags: ["Html","Css","javascript"],
         logos: ["fab fa-html5","fab fa-css3","fab fa-js"],
         code: 'https://github.com/nagesh199/Nykaa-clone',
-        demo: 'https://sage-biscochitos-75e444.netlify.app',
+        demo: 'https://nykaa-clone-dusky.vercel.app/',
         image: "https://images-static.nykaa.com/uploads/691d21d2-0a37-4362-8492-e595668f3586.jpg?tr=w-1200,cm-pad_resize"
     },
         
     {
-        id: 3,
+        id: 4,
         projectName: 'credo-beauty-clone',
         projectDesc: ' A web application to buy makeup and screen care products.',
         tags: ["Html", "css", "javascript"],
@@ -31,6 +43,16 @@ export const projectsData = [
         code: 'https://github.com/nagesh199/credo-beauty-clone',
         demo: 'https://extraordinary-puppy-6b97c8.netlify.app/',
         image: "https://cdn.shopify.com/s/files/1/0637/6147/files/HP_Desktop_1400x528_REV2_1920x.jpg?v=1643680346"
+    },
+    {
+        id: 5,
+        projectName: ' Cricinfo-clone',
+        projectDesc: ' Cricinfo is a sports news website exclusively for the game of cricket. The site features news, articles, live coverage of cricket matches.',
+        tags: ["React", "Redux","css","EJS", "Node.js", "mongoDB"],
+        logos: ["fab fa-css3","fab fa-vuejs", "fas fa-database", "fab fa-js", "fab fa-node"],
+        code: 'https://github.com/nagesh199/Cricinfo-Clone',
+        demo: 'https://espncricinfo-clone.netlify.app/',
+        image: "https://getsarkarinaukari.co.in/wp-content/uploads/2022/06/1655436477_photo.jpg"
     },
     // {
     //     id: 5,
